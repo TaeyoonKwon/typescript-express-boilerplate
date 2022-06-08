f764407e554f0777fd64a30763d98e08c957c6f5
Taeyoon Kwon <47032283+TaeyoonKwon@users.noreply.github.com>
2022-06-08 17:35:10 +0900

    update jwt utility functions

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -11,19 +11,27 @@ export const jwtSign = ({
 }) =>
   JWT.sign(payload, process.env.JWT_SECRET as string, {
     subject,
-    expiresIn: "2h",
+    expiresIn: "1h",
     algorithm: "HS256",
   });
 
 export const jwtVerify = ({ token }: { token: string }) =>
   JWT.verify(token, process.env.JWT_SECRET as string);
 
-export const jwtRefresh = ({ subject = "user" }: { subject: string }) =>
-  JWT.sign({}, process.env.JWT_SECRET as string, {
+export const jwtRefreshSign = async ({
+  user,
+  subject = "user",
+}: {
+  user: string;
+  subject?: string;
+}) => {
+  const token = JWT.sign({}, process.env.JWT_SECRET as string, {
     subject,
     expiresIn: "14d",
     algorithm: "HS256",
   });
+  await UserRefreshToken.create({ token, user });
+};
 
 export const jwtRefreshVerify = async ({
   token,
@@ -33,7 +41,10 @@ export const jwtRefreshVerify = async ({
   user: string;
 }) => {
   const userRefreshTokenDoc = await UserRefreshToken.findOne({ token, user });
+
   if (!userRefreshTokenDoc) throw Error(`Invalid refresh token.`);
+
+  await userRefreshTokenDoc.remove();
 
   return jwtVerify({ token });
 };
